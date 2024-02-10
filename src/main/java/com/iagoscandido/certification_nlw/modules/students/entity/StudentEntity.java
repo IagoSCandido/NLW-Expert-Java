6dc9e71cc242d605786688b9d88848ee58522f49
iago silva <iago.scandido3@gmail.com>
2024-02-10 00:36:59 -0300

    segunda aula

--- src/main/java/com/iagoscandido/certification_nlw/modules/students/entity/StudentEntity.java
+++ src/main/java/com/iagoscandido/certification_nlw/modules/students/entity/StudentEntity.java
@@ -1,6 +1,5 @@
 package com.iagoscandido.certification_nlw.modules.students.entity;
 
-
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -8,13 +7,23 @@ import lombok.NoArgsConstructor;
 import java.util.List;
 import java.util.UUID;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@Entity(name = "students")
 public class StudentEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
     private UUID id;
+    @Column(name = "email", unique = true, nullable = false)
     private String email;
-    private List<CertificationStudentEntity> certificationStudentEntity;
 
+    // private List<CertificationStudentEntity> certificationStudentEntity;
 
 }
