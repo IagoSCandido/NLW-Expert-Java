e460f622a46609bd34acc0514e041c2b532028dc
iago silva <iago.scandido3@gmail.com>
2024-02-11 23:32:21 -0300

    segunda aula

--- src/main/java/com/iagoscandido/certification_nlw/modules/students/repositories/ICertificationStudentRepository.java
+++ src/main/java/com/iagoscandido/certification_nlw/modules/students/repositories/ICertificationStudentRepository.java
@@ -12,6 +12,6 @@ import com.iagoscandido.certification_nlw.modules.students.entity.CertificationS
 @Repository
 public interface ICertificationStudentRepository extends JpaRepository<CertificationStudentEntity, UUID> {
 
-    @Query("SELECT c FROM certifications c INNER JOIN c.studentEntity std WHERE std.email = :email AND c.technology = :technology")
+    @Query(nativeQuery = true, value = "SELECT c FROM certifications c INNER JOIN c.studentEntity std WHERE std.email = :email AND c.technology = :technology")
     List<CertificationStudentEntity> findByStudentEmailAndTechnology(String email, String technology);
 }
--- src/main/java/com/iagoscandido/certification_nlw/modules/students/useCases/VerifyIfHasCertificationUseCase.java
+++ src/main/java/com/iagoscandido/certification_nlw/modules/students/useCases/VerifyIfHasCertificationUseCase.java
@@ -20,5 +20,6 @@ public class VerifyIfHasCertificationUseCase {
             return true;
         }
         return false;
+
     }
 }
